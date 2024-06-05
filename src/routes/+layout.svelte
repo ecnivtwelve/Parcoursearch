d738dde03d1afdf23b1fcb71654a79b00fe55792
Vince Linise <32978709+ecnivtwelve@users.noreply.github.com>
2024-06-05 07:34:42 +0000

    Ajout des principaux styles

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<slot></slot>
+
+<style>
+  @import '../styles/global.css';
+  @import '../styles/search.css';
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,51 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { Search, GraduationCap, Route, MapPin } from 'lucide-svelte';
+</script>
+
+<div class="app">
+  <div class="pane pane-search">
+    
+    <div class="search-form">
+      <div class="search-input">
+        <GraduationCap size="20" />
+        <input
+          type="text"
+          placeholder="Rechercher une formation" 
+          autocomplete="off"
+        />
+      </div>
+      <div class="search-group">
+        <div class="search-input">
+          <MapPin size="20" />
+          <input
+            type="text"
+            placeholder="Ville" 
+            autocomplete="off"
+          />
+        </div>
+        <div class="search-input input-dist">
+          <Route size="20" />
+          <input
+            type="number"
+            placeholder="Dist." 
+            autocomplete="off"
+          />
+          <p class="hlpr">km</p>
+        </div>
+      </div>
+      <div class="search-button">
+        <Search size="20" />
+        <p>Rechercher</p>
+      </div>
+    </div>
+
+  </div>
+
+  <div class="pane pane-results">
+  
+  </div>
+
+  <div class="pane pane-map">
+  
+  </div>
+</div>
